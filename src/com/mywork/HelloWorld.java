d4029202ef74ab00cca9dc0197dae3a68a27d5b0
Mohit Gupta <mhg2491@gmail.com>
2019-07-11 21:53:28 +0530

    initial commit

--- src/com/mywork/HelloWorld.java
+++ src/com/mywork/HelloWorld.java
@@ -1,11 +1,8 @@
-import java.util.ArrayList;
+package com.mywork;
 
 public class HelloWorld {
 
     public static void main(String[] args){
         System.out.println("This is my first java program on mac pro :)");
-        ArrayList dogList = new ArrayList();
-        System.out.println(dogList);
     }
 }
-
